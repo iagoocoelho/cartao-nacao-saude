3e11891a4908524c53fbb4d29a1ead73bccee878
iago.coelho <iagoocoelho@gmail.com>
2022-11-26 18:58:34 -0300

    mock packages

--- src/store/packages/index.js
+++ src/store/packages/index.js
@@ -5,7 +5,213 @@ const INITIAL_STATE = {
         loading: false,
         success: false,
         error: false,
-        packages: [],
+        packages: [
+            {
+                id: 1,
+                name: "Individual",
+                description: "Individual",
+                payment_period: "M",
+                price: 24.9,
+                max_dependents: 0,
+                active: true,
+                package_details: [
+                    {
+                        id: 1,
+                        description: "Sem direito a dependentes/adicionais",
+                        included: false
+                    },
+                    {
+                        id: 2,
+                        description: "Telemedicina Einstein Conecta 24h",
+                        included: true
+                    },
+                    {
+                        id: 3,
+                        description: "Seguros: Vida, assistência funerária, acidentes pessoais e invalidez",
+                        included: true
+                    },
+                    {
+                        id: 4,
+                        description: "Descontos de até 40% nas escolas e universidades do Grupo Cruzeiro do Sul",
+                        included: true
+                    },
+                    {
+                        id: 5,
+                        description: "Descontos de até 30% em farmácias",
+                        included: false
+                    },
+                    {
+                        id: 6,
+                        description: "Descontos de até 80% em consultas e exames",
+                        included: false
+                    },
+                    {
+                        id: 7,
+                        description: "Concorra a R$5.000,00 todos os meses.",
+                        included: true
+                    },
+                    {
+                        id: 8,
+                        description: "Sem rede credenciada",
+                        included: false
+                    }
+                ]
+            },
+            {
+                id: 2,
+                customer_type_id: 1,
+                name: "Individual Plus",
+                description: "Individual Plus",
+                payment_period: "M",
+                price: 37.9,
+                max_dependents: 0,
+                active: true,
+                package_details: [
+                    {
+                        id: 9,
+                        description: "Sem direito a dependentes/adicionais",
+                        included: false
+                    },
+                    {
+                        id: 10,
+                        description: "Telemedicina Einstein Conecta 24h",
+                        included: true
+                    },
+                    {
+                        id: 11,
+                        description: "Seguros: Vida, assistência funerária, acidentes pessoais e invalidez",
+                        included: true
+                    },
+                    {
+                        id: 12,
+                        description: "Descontos de até 40% nas escolas e universidades do Grupo Cruzeiro do Sul",
+                        included: true
+                    },
+                    {
+                        id: 13,
+                        description: "Descontos de até 30% em farmácias",
+                        included: true
+                    },
+                    {
+                        id: 14,
+                        description: "Descontos de até 80% em consultas e exames",
+                        included: true
+                    },
+                    {
+                        id: 15,
+                        description: "Concorra a R$5.000,00 todos os meses.",
+                        included: true
+                    },
+                    {
+                        id: 16,
+                        description: "Sem rede credenciada",
+                        included: true
+                    }
+                ]
+            },
+            {
+                id: 3,
+                name: "Familiar",
+                description: "Familiar",
+                payment_period: "M",
+                price: 79.9,
+                max_dependents: 3,
+                active: true,
+                package_details: [
+                    {
+                        id: 17,
+                        description: "Sem direito a dependentes/adicionais",
+                        included: true
+                    },
+                    {
+                        id: 18,
+                        description: "Telemedicina Einstein Conecta 24h",
+                        included: true
+                    },
+                    {
+                        id: 19,
+                        description: "Seguros: Vida, assistência funerária, acidentes pessoais e invalidez",
+                        included: true
+                    },
+                    {
+                        id: 20,
+                        description: "Descontos de até 40% nas escolas e universidades do Grupo Cruzeiro do Sul",
+                        included: true
+                    },
+                    {
+                        id: 21,
+                        description: "Descontos de até 30% em farmácias",
+                        included: false
+                    },
+                    {
+                        id: 22,
+                        description: "Descontos de até 80% em consultas e exames",
+                        included: false
+                    },
+                    {
+                        id: 23,
+                        description: "Concorra a R$5.000,00 todos os meses.",
+                        included: true
+                    },
+                    {
+                        id: 24,
+                        description: "Sem rede credenciada",
+                        included: false
+                    }
+                ]
+            },
+            {
+                id: 4,
+                name: "Familiar Plus",
+                description: "Familiar Plus",
+                payment_period: "M",
+                price: 97,
+                max_dependents: 3,
+                active: true,
+                package_details: [
+                    {
+                        id: 25,
+                        description: "Sem direito a dependentes/adicionais",
+                        included: true
+                    },
+                    {
+                        id: 26,
+                        description: "Telemedicina Einstein Conecta 24h",
+                        included: true
+                    },
+                    {
+                        id: 27,
+                        description: "Seguros: Vida, assistência funerária, acidentes pessoais e invalidez",
+                        included: true
+                    },
+                    {
+                        id: 28,
+                        description: "Descontos de até 40% nas escolas e universidades do Grupo Cruzeiro do Sul",
+                        included: true
+                    },
+                    {
+                        id: 29,
+                        description: "Descontos de até 30% em farmácias",
+                        included: true
+                    },
+                    {
+                        id: 30,
+                        description: "Descontos de até 80% em consultas e exames",
+                        included: true
+                    },
+                    {
+                        id: 31,
+                        description: "Concorra a R$5.000,00 todos os meses.",
+                        included: true
+                    },
+                    {
+                        id: 32,
+                        description: "Sem rede credenciada",
+                        included: true
+                    }
+                ]
+            }
+        ]
     },
     register: {
         loading: false,
@@ -20,6 +226,7 @@ const reducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 data: {
+                    ...state.data,
                     loading: true,
                     success: false,
                     error: false,
